e4dd55a10eb03ba1ad0729a1f3778ad13ad1d080
YassineOH <yassine.yas98@gmail.com>
2022-08-04 23:31:13 +0100

    clear all console logs

--- src/app/sendData.ts
+++ src/app/sendData.ts
@@ -9,20 +9,17 @@ export interface Response {
 const send = async (): Promise<Response> => {
   const data = store.getState();
   const { user, service } = data;
-  console.log(import.meta.env.VITE_URL_API);
-  console.log(import.meta.env);
   try {
-    const response = await axios.post(import.meta.env.VITE_URL_API, {
+    await axios.post(import.meta.env.VITE_URL_API, {
       user,
       service,
     });
-    console.log(response);
+
     return {
       type: "success",
       msg: "Nous avons reçu votre demande",
     };
   } catch (error) {
-    console.log(error);
     return {
       type: "danger",
       msg: "quelque chose s'est mal passé, veuillez réessayer plus tard",
